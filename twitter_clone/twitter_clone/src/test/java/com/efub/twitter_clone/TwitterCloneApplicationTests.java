c31b5544ddeb695fc61460c2cc4fdd81afb88d72
SEON UI LEE <sunnyinsummer@ewhain.net>
2022-05-18 22:41:34 +0900

    first commit

---
+++ twitter_clone/twitter_clone/src/test/java/com/efub/twitter_clone/TwitterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efub.twitter_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
